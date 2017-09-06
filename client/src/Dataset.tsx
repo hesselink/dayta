276fa51c13cd3b082fcde544b37ab70bf6149eef
Erik Hesselink <hesselink@gmail.com>
2017-09-06 20:44:01 +0200

    Add simple data item save form.

--- client/src/Dataset.tsx
+++ client/src/Dataset.tsx
@@ -5,8 +5,15 @@ import "whatwg-fetch"
 interface DatasetProps {
   user : string
   name : string
-  status : "loading" | "loaded" | "failed"
+  loadStatus : "loading" | "loaded" | "failed"
+  saveStatus : "none" | "saving" | "saved" | "failed"
   data : Array<DataItem>
+  saveDataItem : (dataItem : DataItem) => void
+}
+
+interface DatasetState {
+  date : string
+  number : string
 }
 
 export interface DataItem {
@@ -14,20 +21,101 @@ export interface DataItem {
   value : number
 }
 
-export class Dataset extends React.Component<DatasetProps> {
+export class Dataset extends React.Component<DatasetProps, DatasetState> {
+  constructor (props: DatasetProps) {
+    console.log("Dataset constructor", props)
+    super(props);
+    this.state = {
+      date : new Date().toString(),
+      number : ""
+    };
+  }
+
   render () : JSX.Element {
-    let { user, name, status, data } = this.props
-    console.log(status, data)
+    console.log("Dataset.render", this)
+    let { user, name, loadStatus, saveStatus, data } = this.props
+    console.log(loadStatus, data)
     return <div>
              <h2>Hello {user}!</h2>
-             <h3>{ (status == "loading" ? "Loading" :
-                    status == "loaded"  ? "Viewing" :
-                    status == "failed"  ? "Error loading" : "Unknown status for"
-                   ) + " dataset " + name}</h3>
+             <h3>{ (loadStatus == "loading" ? "Loading" :
+                    loadStatus == "loaded"  ? "Viewing" :
+                    loadStatus == "failed"  ? "Error loading" : "Unknown status for"
+                   ) + " dataset " + name}
+             </h3>
              <ul>
              { data.map(item => <DataListItem key={item.datetime} data={item}/>) }
              </ul>
+             <form onSubmit={ e => this.saveDataItem(e) }>
+               <label>
+                 Date: <input type="date" className="date" value={ this.state.date.toString() } onChange={ e => this.dateInputChange(e) } />
+               </label><br />
+               <label>
+                 Value: <input type="number" className="number" value={ this.state.number } onChange={ e => this.numberInputChange(e) } />
+               </label><br />
+               <input type="submit" value="Save" />
+             </form>
+             <h3>{ (saveStatus == "saving" ? "Saving..." :
+                    saveStatus == "saved"  ? "Saved" :
+                    saveStatus == "failed"  ? "Error saving" : ""
+                   ) }
+             </h3>
            </div>
+  }
+
+  dateInputChange (e : React.ChangeEvent<HTMLInputElement>) {
+    let val : string = e.target.value;
+    this.setState( {
+      "date": val
+    })
+  }
+
+  numberInputChange (e : React.ChangeEvent<HTMLInputElement>) {
+    let val : string = e.target.value;
+    this.setState( {
+      "number": val
+    })
+  }
+
+/*
+    let strVal : string = e.target.value;
+    let mVal : number = Date.parse(strVal);
+    if (!isNaN(mVal)) {
+      this.setState( {
+        "date": new Date(mVal)
+      })
+    }
+  }
+
+  numberInputChange (e : React.ChangeEvent<HTMLInputElement>) {
+    let strVal : string = e.target.value;
+    let mVal : number = parseInt(strVal);
+    if (!isNaN(mVal)) {
+      this.setState( {
+        "number": mVal
+      })
+    }
+  }
+*/
+
+  saveDataItem (e : React.FormEvent<HTMLFormElement>) : void {
+    console.log(e);
+    e.preventDefault();
+    let dateInput : HTMLInputElement = e.currentTarget.getElementsByClassName("date")[0] as HTMLInputElement;
+    let numberInput : HTMLInputElement = e.currentTarget.getElementsByClassName("number")[0] as HTMLInputElement;
+    let date : number = Date.parse(dateInput.value);
+    let num : number = parseInt(numberInput.value);
+    let hasError = false;
+    if (isNaN(date)) {
+      hasError = true;
+      // TODO show error
+    }
+    if (isNaN(num)) {
+      hasError = true;
+      // TODO show error
+    }
+    if (!hasError) {
+      this.props.saveDataItem({ datetime: new Date(date).toISOString(), value: num });
+    }
   }
 }
 
--- client/src/DatasetContainer.tsx
+++ client/src/DatasetContainer.tsx
@@ -4,42 +4,89 @@ import { RouteComponentProps } from "react-router-dom"
 import { Dataset, DataItem } from "./Dataset"
 
 interface DatasetContainerState {
-    status : "loading" | "loaded" | "failed"
-    data : Array<DataItem>
+  loadStatus : "loading" | "loaded" | "failed"
+  saveStatus : "none" | "saving" | "saved" | "failed"
+  data : Array<DataItem>
 }
 
 class DatasetContainer extends React.Component<RouteComponentProps<any>> {
   state : DatasetContainerState = {
-    status : "loading",
+    loadStatus : "loading",
+    saveStatus : "none",
     data : []
   }
 
   componentDidMount () {
-    fetch( "/api/user/" + this.props.match.params.user
+    this.reloadData();
+  }
+
+  getItemUrl () : string {
+    return "/api/user/" + this.props.match.params.user
          + "/dataset/" + this.props.match.params.dataset + "/item"
-         ).then(response => response.json())
-         .then(this.renderResponse)
-         .catch(this.renderError)
+  }
+
+  reloadData () {
+    fetch(this.getItemUrl())
+      .then(response => response.json())
+      .then(this.renderResponse.bind(this))
+      .catch(this.renderError.bind(this))
+  }
+
+  saveDataItem (dataItem : DataItem) {
+    this.setState({
+      saveState: "saving"
+    })
+    let headers = new Headers();
+    headers.set("Content-Type", "application/json");
+    fetch( this.getItemUrl()
+         , { method : "POST"
+           , headers: headers
+           , body: JSON.stringify(dataItem)
+           }
+         )
+      .then(response => response.json())
+      .then(this.dataItemSaved.bind(this))
+      .catch(this.dataItemSaveFailed.bind(this))
+    this.reloadData();
+  }
+
+  dataItemSaved (json : any) : void {
+    console.log("Item saved", json);
+    this.setState({
+      saveStatus: "saved"
+    });
+  }
+
+  dataItemSaveFailed (error : Error) : void {
+    console.log("Error saving item", error);
+    this.setState({
+      saveStatus: "failed"
+    });
   }
 
   render () : JSX.Element {
     return <Dataset user={this.props.match.params.user}
                     name={this.props.match.params.dataset}
-                    status={this.state.status}
+                    loadStatus={this.state.loadStatus}
+                    saveStatus={this.state.saveStatus}
                     data={this.state.data}
+                    saveDataItem={this.saveDataItem.bind(this)}
            />
   }
 
-  renderResponse = (json : any) => {
-    console.log(json)
+  renderResponse (json : any) : void {
+    console.log("Loaded data: ", json)
     this.setState((prevState) => ({
-      status: "loaded",
+      loadStatus: "loaded",
       data: json
     }))
   }
 
-  renderError (error : Error) {
-    console.log(error)
+  renderError (error : Error) : void {
+    console.log("Error loading data: ", error)
+    this.setState({
+      loadStatus: "failed"
+    });
   }
 }
 
